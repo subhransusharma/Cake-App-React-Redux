12f53270fff846c31376863a7c4b8deed061b467
subhransu <subhransu.suman@gmail.com>
2022-08-27 08:47:20 -0700

    Updates

---
+++ precache-manifest.1ef9426493435cd2957dea99a61b265e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7073e67b07cc2dc41e58c3445f0f8e",
+    "url": "/Cake-App-React-Redux/index.html"
+  },
+  {
+    "revision": "ee673fb59aaae77d003e",
+    "url": "/Cake-App-React-Redux/static/css/2.a779dd76.chunk.css"
+  },
+  {
+    "revision": "64c96ff57f3cae8de570",
+    "url": "/Cake-App-React-Redux/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ee673fb59aaae77d003e",
+    "url": "/Cake-App-React-Redux/static/js/2.8aa8e1f6.chunk.js"
+  },
+  {
+    "revision": "f0c4eba68c97a04b0bcdd57175cae0f9",
+    "url": "/Cake-App-React-Redux/static/js/2.8aa8e1f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5db1e27ee2f34be8a22",
+    "url": "/Cake-App-React-Redux/static/js/3.794455b0.chunk.js"
+  },
+  {
+    "revision": "64c96ff57f3cae8de570",
+    "url": "/Cake-App-React-Redux/static/js/main.5e2b7863.chunk.js"
+  },
+  {
+    "revision": "b0a04857dc888500ba2c",
+    "url": "/Cake-App-React-Redux/static/js/runtime-main.f80e4467.js"
+  }
+]);
